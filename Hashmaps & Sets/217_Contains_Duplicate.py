0d2e6dc78a12f7f9ce8bfc4cb827ccb27b77adc8
Adam Hajjaji <hajjajiadam.23@gmail.com>
2025-09-01 15:49:06 +0100

    Create 217_Contains_Duplicate.py

---
+++ Hashmaps & Sets/217_Contains_Duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        dic = {}
+        for i in nums:
+            if i in dic:
+                return True
+            else:
+                dic[i]=0
+        return False
