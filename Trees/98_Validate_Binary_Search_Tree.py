09fb587474c1cac0580271dfe26b685442a324c0
Adam Hajjaji <hajjajiadam.23@gmail.com>
2025-09-01 22:10:44 +0100

    Create 98_Validate_Binary_Search_Tree.py

---
+++ Trees/98_Validate_Binary_Search_Tree.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        self.prev = None
+
+        def dfs(node):
+            if not node:
+                return True
+            if not dfs(node.left):
+                return False
+            if self.prev is not None and node.val <= self.prev:
+                return False
+            self.prev = node.val
+            return dfs(node.right)
+
+        return dfs(root)
