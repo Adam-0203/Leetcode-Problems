5a94b1d6e781ff0b80c37b64e82493306c48061a
Adam Hajjaji <hajjajiadam.23@gmail.com>
2025-09-01 14:30:23 +0100

    Update 121_Best_Time_to_Buy_and_Sell_Stock_solution2.py

--- Arrays and Strings/121_Best_Time_to_Buy_and_Sell_Stock_solution2.py
+++ Arrays and Strings/121_Best_Time_to_Buy_and_Sell_Stock_solution2.py
@@ -1,18 +1,15 @@
-prices = [2,1000000,1,5,3,6,4]
-
-def profit(lista):
-    return max(lista)-lista[0]
-
-
-def max_profit(lista):
-    if len(lista) == 2: 
-        if lista[1]-lista[0]<0:
-            return 0
-        return lista[1]-lista[0]
-    elif len(lista)<=1: return 0
+class Solution(object):
+    def maxProfit(self, prices):
+        def profit(lista):
+            return max(lista)-lista[0]
 
 
-    return max(max_profit(lista[:lista.index(min(lista))]),
-               profit(lista[lista.index(min(lista)):]))
+        def max_profit(lista):
+            if len(lista) == 2: return lista[1]-lista[0]
+            elif len(lista)<=1: return 0
+            return max(max_profit(lista[:lista.index(min(lista))]),profit(lista[lista.index(min(lista)):]))
 
-print(max_profit(prices))
+        
+        res = max_profit(prices)
+        if res>0: return res
+        return 0
