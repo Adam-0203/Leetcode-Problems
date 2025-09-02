689d16155549dec93d443764092e425522a6b923
Adam Hajjaji <hajjajiadam.23@gmail.com>
2025-09-02 10:06:52 +0100

    Create 78_Subsets.py

---
+++ Recursive Backtracking/78_Subsets.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def subsets(self, nums):
+        res = [[]]
+        for num in nums:
+            for i in range(len(res)):
+                res.append(res[i]+[num])
+        
+        return res
